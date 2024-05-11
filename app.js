76eb2a1fc34e66722b0471a9e78c0224c7192906
Юлия Тюрина <juliosoctober@gmail.com>
2024-05-11 19:37:06 +0300

    database added

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js 
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const gamesRouter = require('./routes/games')
+const categoriesRouter = require('./routes/categories')
+const usersRouter = require('./routes/users')
+
+const cors = require("./middlewares/cors")
+const connectToDatabase = require('./database/connect')
+
+const PORT = 3000;
+const app = express();
+
+connectToDatabase()
+
+app.use(
+  cors,
+  bodyParser.json(),
+  express.static(path.join(__dirname, 'public')),
+  gamesRouter,
+  categoriesRouter,
+  usersRouter,
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
